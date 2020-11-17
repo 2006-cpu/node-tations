cfe5f47e9c7f48438ad6d93202cde0fa4e1ba18b
Travis Pinkerton <travispinkerton@hotmail.com>
2020-11-17 10:39:41 -0800

    API connected: products route established

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 const apiRouter = require('express').Router();
 
+const {getAllProducts, getProductById} = require('../db/products')
+// import {getProducts} from ('../src/api')
 apiRouter.get("/", (req, res, next) => {
   res.send({
     message: "API is under construction!"
   });
 });
 
-module.exports = apiRouter;
+apiRouter.get("/products", async (req, res, next) => {
+  try {
+    const products = await getAllProducts();
+    console.log("products:", products)
+    res.send({
+      message: products
+    });
+  } catch (error) {
+
+  }
+
+});
+
+apiRouter.get("/product/:productId", async (req, res, next) => {
+  const { productId } = req.params
+  try {
+    const products = await getProductById(productId);
+    console.log("products:", products)
+    res.send({
+      message: products
+    });
+  } catch (error) {
+
+  }
+
+});
+
+module.exports = apiRouter
