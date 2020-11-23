5f341cc4d334aeacb3ca386403c784a490e2089f
Travis Pinkerton <travispinkerton@hotmail.com>
2020-11-22 22:32:34 -0800

    single view for product card added

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 
 import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Header, Catalog } from './components';
+import { Header, Catalog, SoloCard, ProductCard } from './components';
 
 const App = () => {
+	const [products, setProducts] = useState([]);
+	const [product, setProduct] = useState({});
+	console.log("product:", product)
+	const [productId, setProductId] = useState({});
 	return (
 		<Router>
 			<ChakraProvider>
@@ -17,9 +21,14 @@ const App = () => {
 								Welcome to Title of Website
 							</h2>
 						</Route>
-						<Route path='/products'>
-							<Catalog />
+						<Route exact path='/products'>
+							<Catalog products={products} setProducts={setProducts} product={product} setProduct={setProduct} productId={productId} setProductId={setProductId} />
 						</Route>
+						<Route exact path='/viewitem' >
+							<SoloCard productId={productId} product={product} setProduct={setProduct} setProductId={setProductId} products={products} setProducts={setProducts}/>
+						</Route>
+						
+						
 					</Switch>
 				</div>
 			</ChakraProvider>
