9ba6865026ddb286a0ecaee3151e52f92fb89842
beto <albert.4770@gmail.com>
2020-12-06 07:36:34 -0600

    stripe checkout feature

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,8 +1,49 @@
 const stripeRouter = require('express').Router();
-const stripe = require('stripe');
+const { LOCAL_URL, STRIPE_KEY } = process.env;
+const stripe = require('stripe')(STRIPE_KEY);
+const { requireUser } = require('./utils');
+const { getCartByUser } = require('../db/orders');
 
-stripeRouter.post('/create-session', (req, res, next) => {
-	res.send('wasup');
+stripeRouter.post('/create-session', requireUser, async (req, res, next) => {
+	const { id } = req.user;
+
+	const session = await stripe.checkout.sessions.create({
+		payment_method_types: ['card'],
+		line_items: await buildLineItems(id),
+		mode: 'payment',
+		success_url: `${LOCAL_URL + '/cart'}`,
+		cancel_url: `${LOCAL_URL + '/cart'}`
+	});
+
+	res.send({ id: session.id });
 });
+
+const convertToCents = total => {
+	cents = total * 100;
+	Math.trunc(cents);
+	return cents;
+};
+
+const buildLineItems = async userId => {
+	const [cartData] = await getCartByUser(userId);
+
+	let stripeItems = [];
+
+	cartData.products.forEach(product => {
+		stripeItems.push({
+			price_data: {
+				currency: 'usd',
+				product_data: {
+					name: product.name,
+					images: [product.imageurl]
+				},
+				unit_amount: convertToCents(product.price)
+			},
+			quantity: Math.trunc(product.quantity)
+		});
+	});
+
+	return stripeItems;
+};
 
 module.exports = { stripeRouter };
